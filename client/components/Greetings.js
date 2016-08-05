28901113b567fa91dc74d95ba64204aae67bcb8a
Daniel Pope <dpope@dminc.com>
2016-08-05 10:59:24 -0400

    setup router

--- client/components/Greetings.js
+++ client/components/Greetings.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-export default  () => {
-     return (
-       <h1>h!!</h1>
-     );
+class Greetings extends React.Component {
+  render () {
+    return (
+      <div className="jumbotron">
+         <h1>hello</h1>
+      </div>
+    );
+  }
 }
+export default  Greetings;
