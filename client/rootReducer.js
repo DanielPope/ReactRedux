d7a009a2edcfd8d6b815cbe8135746630c8a2f06
Daniel Pope <dpope@dminc.com>
2016-08-09 00:19:31 -0400

    renders flash messages and allows to delete them

--- client/rootReducer.js
+++ client/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import flashMessages from './reducers/FlashMessages';
+import flashMessages from './reducers/flashMessages';
 
 export default combineReducers({
   flashMessages
